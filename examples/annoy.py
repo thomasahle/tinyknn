88a00c0d17114454f41e9cc80f7c8aefc8cb17c7
tahle <ahle@fb.com>
2023-04-11 12:18:24 -0700

    Added comparison to Anony

---
+++ examples/annoy.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import argparse
+import time
+import numpy as np
+import os.path
+import sys
+import pickle
+import tqdm
+
+from annoy import AnnoyIndex
+from fast_pq import cdist, brute
+
+parser = argparse.ArgumentParser(description="Benchmark FastPQ and IVF on GloVe dataset")
+parser.add_argument("filename", help="Path to the GloVe .npy file (e.g., glove.840B.100d.npy)")
+parser.add_argument(
+    "--n-queries",
+    type=int,
+    default=10000,
+    help="Number of random queries to run (default: 10000)",
+)
+parser.add_argument(
+    "--dims-per-block",
+    type=int,
+    default=2,
+    help="More dims-per-block is faster, but less precise (default: 2)",
+)
+parser.add_argument(
+    "--k-neighbours",
+    type=int,
+    default=10,
+    help="Number of neighbours in k-NN search (default: 10)"
+)
+parser.add_argument(
+    "--query-method",
+    choices=["query", "query2", "query3"],
+    default="query",
+    help="Select the query method to use: query, query2, or query3 (default: query)"
+)
+parser.add_argument(
+    "--metric",
+    choices=["euclidean", "angular"],
+    default="euclidean",
+    help="Metric to use in IVF. Can be 'euclidean' or 'angular'. Default is 'euclidean'."
+)
+args = parser.parse_args()
+
+num_queries, dims_per_block, k_neighbours = args.n_queries, args.dims_per_block, args.k_neighbours
+print("Loading and shuffling...")
+data = np.load(args.filename)
+np.random.seed(10)
+np.random.shuffle(data)
+data, queries = data[:-num_queries], data[-num_queries:]
+
+num_points, num_dims = data.shape
+num_clusters = int(num_points**0.5)
+print(f"{num_points=}, {num_dims=}, {num_queries=}, {dims_per_block=}, {num_clusters=}")
+
+true_neighbours_filename = f"trus_{num_points}_{num_queries}_{args.metric}.npy"
+#true_neighbours_filename = f"trus_{num_points}_{num_queries}.npy"
+if os.path.isfile(true_neighbours_filename):
+    print("Loading true neighbours from", true_neighbours_filename)
+    true_neighbours = np.load(true_neighbours_filename)
+    num_queries, k_neighbours = true_neighbours.shape
+    print(f"Found {num_queries=}, {k_neighbours=}")
+else:
+    print("Computing true neighbours...")
+    start = time.time()
+    true_neighbours = brute(queries, data, k_neighbours, metric=args.metric)
+    print(f"Took {time.time() - start:.1f} seconds.")
+    np.save(true_neighbours_filename, true_neighbours)
+
+
+print("Now that we have the index, actually add the points to it.")
+for build_probes in [100, 200, 400]:
+    print("Creating new Annoy index...")
+    start = time.time()
+    ann = AnnoyIndex(data.shape[1], 'angular')
+    for i, v in enumerate(tqdm.tqdm(data)):
+        ann.add_item(i, v)
+    print(f"Took {time.time() - start:.1f} seconds.")
+
+    print(f"Building {build_probes} trees...")
+    start = time.time()
+    ann.build(build_probes)
+    print(f"Took {time.time() - start:.1f} seconds.")
+
+    print("Querying")
+    recall = 0
+    for n_probes in [100, 200, 400, 1000, 2000, 4000, 10000, 20000, 40000, 100000, 200000, 400000]:
+        start = time.time()
+        found = 0
+
+        with tqdm.tqdm(enumerate(zip(queries, true_neighbours)), total=num_queries, leave=False) as pbar:
+            pbar.set_description(f"Probing: {n_probes} nodes")
+            for i, (query, true_neighbor) in pbar:
+                guess = ann.get_nns_by_vector(query, n=k_neighbours, search_k=n_probes)
+                found += len(set(true_neighbor) & set(guess))
+
+        query_time = time.time() - start
+        recall = found / k_neighbours / num_queries
+
+        print(f"Recall{k_neighbours}@{k_neighbours}:", recall)
+        qps = num_queries / query_time
+        print("Queries/second:", qps)
+        if qps < 500:
+            break
+
--- examples/plot_bench.py
+++ examples/plot_bench.py
@@ -4,13 +4,16 @@ import matplotlib.pyplot as plt
 data = sys.stdin.read()
 
 # Extract recall and queries/second values from the data
-series = []
+fast_pq_series = []
+annoy_series = []
 
 for line in data.split("\n"):
-    if match := re.match(r"Adding each point to (\d) lists", line):
-        recall = []
-        queries_per_second = []
-        series.append((int(match.group(1)), recall, queries_per_second))
+    if match := re.search(r"Adding each point to (\d+) lists", line):
+        recall, queries_per_second = [], []
+        fast_pq_series.append((int(match.group(1)), recall, queries_per_second))
+    if match := re.search(r"Building (\d+) trees", line):
+        recall, queries_per_second = [], []
+        annoy_series.append((int(match.group(1)), recall, queries_per_second))
     if (match := re.search(r"Recall10@10: (\d+\.\d+)", line)):
         recall_value = float(match.group(1))
         recall.append(recall_value)
@@ -19,10 +22,14 @@ for line in data.split("\n"):
         queries_per_second.append(qps_value)
 
 # Plot the data
-for l, recall, queries_per_second in series[1:2]:
-    plt.plot(recall, queries_per_second, marker='o', label=f"{l} build probes")
+for l, recall, queries_per_second in fast_pq_series[:2]:
+    plt.plot(recall, queries_per_second, marker='o', label=f"FastPQ {l} build probes")
+for l, recall, queries_per_second in annoy_series:
+    plt.plot(recall, queries_per_second, marker='o', label=f"Annoy {l} trees")
 plt.xlabel("Recall10@10")
+plt.xlim([.3, .93])
+plt.ylim([100, 5000])
 plt.ylabel("Queries/second")
 plt.title("Queries/second vs Recall on GloVe 100d angular")
-#plt.legend()
+plt.legend()
 plt.show()
