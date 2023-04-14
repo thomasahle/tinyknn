f4b408ba879111ee91b76736242fadbbbeb1b174
tahle <ahle@fb.com>
2023-04-14 13:47:28 -0700

    Added an angular test

--- tests/test_multiprobe.py
+++ tests/test_multiprobe.py
@@ -1,5 +1,5 @@
 import numpy as np
-from fast_pq import IVF, FastPQ, cdist
+from fast_pq import IVF, FastPQ, brute
 
 np.random.seed(10)
 
@@ -14,7 +14,7 @@ qs = np.random.randn(nq, d).astype(np.float32)
 
 def compute_recall(metric, build_probes, query_probes):
     if at < n:
-        trus = cdist(qs, X).argpartition(axis=1, kth=at)[:, :at]
+        trus = brute(qs, X, k=at, metric=metric)
     else:
         trus = np.broadcast_to(np.arange(n), (nq, n))
     ivf = IVF(metric, int(n**0.5), FastPQ(2))
@@ -25,13 +25,13 @@ def compute_recall(metric, build_probes, query_probes):
         recall_at += len(set(guess) & set(tru))
     return recall_at / nq / at
 
-def test_monotone():
+def _test_monotone(metric):
     n = 4
     table = []
     for build_probes in range(1, n+1):
         table.append([])
         for query_probes in range(1, n+1):
-            recall = compute_recall("euclidean", build_probes, query_probes)
+            recall = compute_recall(metric, build_probes, query_probes)
             table[-1].append(recall)
 
     for row in table:
@@ -46,3 +46,10 @@ def test_monotone():
     for i in range(n):
         for j in range(1, n):
             assert table[i][j] >= table[i][j-1] - 0.1
+
+def test_euclidean():
+    _test_monotone("euclidean")
+
+def test_angular():
+    _test_monotone("angular")
+
