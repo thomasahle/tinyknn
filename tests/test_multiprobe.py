thomasahle/tinyknn

commit c6a03baca6051fde9f1f34c2e6e64a33313a582c

Faster index building

examples/multiprobes.py
@@ -1,6 +1,6 @@
 import numpy as np
 import time
-from fast_pq import IVF, cdist
+from fast_pq import IVF, brute
 
 np.random.seed(10)
 
@@ -16,8 +16,8 @@ qs = np.random.randn(nq, d).astype(np.float32)
 
 
 def compute_recall(metric, build_probes, query_probes):
-    if at < n:
-        trus = cdist(qs, X).argpartition(axis=1, kth=at)[:, :at]
+    if at <= n:
+        trus = brute(qs, X, at)
     else:
         trus = np.broadcast_to(np.arange(n), (nq, n))
     ivf = IVF(metric, int((n * build_probes) ** 0.5))

fast_pq/ivf.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sklearn.cluster
 from fast_pq import FastPQ
-from .utils import bottom_k_2d, cdist, timer, brute1, group_data_by_indices
+from .utils import bottom_k_2d, timer, brute1, brute, group_data_by_indices
 from ._fast_pq import query_pq_sse
 
 
@@ -78,23 +78,20 @@ class IVF:
         self.data = data = X.copy()
 
         with timer(verbose, "Computing nearest clusters..."):
-            # TODO: Use the compression here, somehow.
+            nearest_indices = brute(data, self.all_centers, k=n_probes, metric=self.metric)
 
-            if self.metric == "euclidean":
-                distances = cdist(data, self.all_centers)
-            elif self.metric == "angular":
-                data /= np.linalg.norm(data, axis=1, keepdims=True)
-                distances = -data @ self.all_centers.T
-
-            nearest_indices = bottom_k_2d(distances, n_probes)
-
-        # We make sure that all centers have at least one point. This is just
-        # a simple optimiation that's useful when there's not a lot of data
-        # in the IVF, or if the query is OOD.
         with timer(verbose, "PQ Transforming active centers..."):
+            # We make sure that all centers have at least one point. This is just
+            # a simple optimiation that's useful when there's not a lot of data
+            # in the IVF, or if the query is OOD.
+            print(nearest_indices.shape)
+            print(nearest_indices)
+            print(np.unique(nearest_indices).shape)
             self.active_centers = np.ascontiguousarray(
                 self.all_centers[np.unique(nearest_indices)], dtype=np.float32
             )
+            # We could have transformed the centers in IVF.fit(...), but we do it here
+            # instead so we only have to transform the active centers.
             self.pq_transformed_centers = self.pq.transform(self.active_centers)
 
         with timer(verbose, "Transforming points..."):

fast_pq/utils.py
@@ -68,13 +68,14 @@ def brute(X, Y, k, metric="euclidean", chunk=100):
     Computes the k-nearest neighbors for each point in X based on the squared Euclidean distances
     between X and Y.
     """
+    assert k <= Y.shape[0], f"Can't find knn with {k=} and {Y.shape[0]} targets."
     if metric == "angular":
         X = X / np.linalg.norm(X, axis=1, keepdims=True)
         Y = Y / np.linalg.norm(Y, axis=1, keepdims=True)
     elif metric not in ["angular", "euclidean"]:
         raise ValueError(f"Metric not supported: {metric}")
     n = X.shape[0]
-    res = np.zeros((n, k))
+    res = np.zeros((n, k), dtype=int)
     Ynorm2 = np.einsum("ij,ij->i", Y, Y)
     for i in range(0, n, chunk):
         Xchunk = X[i : i + chunk]

examples/example.py
@@ -5,7 +5,7 @@ import numpy as np
 import tqdm
 import argparse
 
-from fast_pq import FastPQ, cdist
+from fast_pq import FastPQ, brute
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--n", type=int, default=160_000, help="Number of samples")
@@ -27,7 +27,7 @@ qs = np.random.randn(k, d).astype(np.float32)
 
 print("Computing true neighbours")
 start = time.time()
-trus = cdist(qs, X).argmin(axis=1)
+trus = brute(qs, X, k=1)[:, 0]
 t0 = time.time() - start
 
 print("Fitting PQ")

tests/test_utils.py
@@ -1,11 +1,15 @@
+import pytest
 import numpy as np
+from itertools import product
 
 from fast_pq import cdist, brute, group_data_by_indices
 
 
+np.random.seed(10)
+
+
 def test_cdist():
     n1, n2, d = 10, 8, 5
-    np.random.seed(10)
     for chunk in [1, 10, 100]:
         X = np.random.randn(n1, d)
         Y = np.random.randn(n2, d)
@@ -16,19 +20,23 @@ def test_cdist():
                 assert np.isclose(dists[i, j], tru_dist)
 
 
-def test_brute():
-    n1, n2, d = 40, 28, 5
-    np.random.seed(10)
+@pytest.mark.parametrize(
+    "n1, n2, d, k",
+    product([40], [28], [5], [0, 1, 10, 28])
+)
+def test_brute(n1, n2, d, k):
     X = np.random.randn(n1, d)
     Y = np.random.randn(n2, d)
-    expected = cdist(X, Y).argpartition(axis=1, kth=10)[:, :10]
-    best = brute(X, Y, 10)
+    if k < n2:
+        expected = cdist(X, Y).argpartition(axis=1, kth=k)[:, :k]
+    else:
+        expected = np.broadcast_to(np.arange(n2), (n1, n2))
+    best = brute(X, Y, k)
     assert np.all(np.sort(expected) == np.sort(best))
 
 
 def test_angular():
     n1, n2, d = 40, 28, 5
-    np.random.seed(10)
     X = np.random.randn(n1, d)
     Y = np.random.randn(n2, d)
     X /= np.linalg.norm(X, axis=1, keepdims=True)

commit 4a5d1f1d62b3c5b2afd27c20c37c39ad36f1756f

Fixed angular regression from c6a03bac

examples/bench.py
@@ -10,7 +10,7 @@ import re
 import sklearn.metrics
 
 from fast_pq import FastPQ
-from fast_pq import IVF, brute
+from fast_pq import IVF, brute, utils
 
 parser = argparse.ArgumentParser(
     description="Benchmark FastPQ and IVF on GloVe dataset"
@@ -75,15 +75,12 @@ true_neighbours_filename = (
     f"trus_{simple_name}_{k_neighbours=}_{num_queries=}_{metric=}.npy"
 )
 if os.path.isfile(true_neighbours_filename):
-    print("Loading true neighbours from", true_neighbours_filename)
-    true_neighbours = np.load(true_neighbours_filename)
+    with utils.timer(True, f"Loading true neighbours from {true_neighbours_filename}"):
+        true_neighbours = np.load(true_neighbours_filename)
     num_queries, k_neighbours = true_neighbours.shape
-    print(f"Found {num_queries=}, {k_neighbours=}")
 else:
-    print("Computing true neighbours...")
-    start = time.time()
-    true_neighbours = brute(queries, data, k_neighbours, metric=args.metric)
-    print(f"Took {time.time() - start:.1f} seconds.")
+    with utils.timer(True, "Computing true neighbours..."):
+        true_neighbours = brute(queries, data, k_neighbours, metric=args.metric, chunk=10)
     np.save(true_neighbours_filename, true_neighbours)
 
 ivf_filename = (
@@ -94,12 +91,11 @@ if os.path.isfile(ivf_filename):
     with open(ivf_filename, "rb") as file:
         pq, ivf = pickle.load(file)
 else:
-    print("Building Index...")
-    pq = FastPQ(dims_per_block)
-    ivf = IVF(args.metric, num_clusters, pq)
-    start = time.time()
-    ivf.fit(data, verbose=True)
-    print(f"Took {time.time() - start:.1f} seconds.")
+    with utils.timer(True, "Building Index..."):
+        pq = FastPQ(dims_per_block)
+        ivf = IVF(args.metric, num_clusters, pq)
+        start = time.time()
+        ivf.fit(data, verbose=True)
     print("Saving index to", ivf_filename)
     with open(ivf_filename, "wb") as file:
         pickle.dump((pq, ivf), file)
@@ -109,10 +105,8 @@ print("Now that we have the index, actually add the points to it.")
 
 n_max_build_probes = 10
 for build_probes in range(1, n_max_build_probes):
-    print(f"Adding each point to {build_probes} lists...")
-    start = time.time()
-    ivf.build(data, n_probes=build_probes, verbose=True)
-    print(f"Took {time.time() - start:.1f} seconds.")
+    with utils.timer(True, f"Adding each point to {build_probes} lists..."):
+        ivf.build(data, n_probes=build_probes, verbose=True)
 
     print("Querying")
     recall = 0

examples/multiprobes.py
@@ -10,6 +10,7 @@ nq = 30
 at = 10
 dpb = 2
 max_probes = 10
+metric = "euclidean"
 
 X = np.random.randn(n, d).astype(np.float32)
 qs = np.random.randn(nq, d).astype(np.float32)
@@ -17,7 +18,7 @@ qs = np.random.randn(nq, d).astype(np.float32)
 
 def compute_recall(metric, build_probes, query_probes):
     if at <= n:
-        trus = brute(qs, X, at)
+        trus = brute(qs, X, at, metric=metric)
     else:
         trus = np.broadcast_to(np.arange(n), (nq, n))
     ivf = IVF(metric, int((n * build_probes) ** 0.5))
@@ -45,7 +46,7 @@ total_query_time = 0
 for build_probes in range(1, max_probes + 1):
     print(f"{build_probes:4}", end=" ")
     for query_probes in range(1, max_probes + 1):
-        recall, query_time = compute_recall("euclidean", build_probes, query_probes)
+        recall, query_time = compute_recall(metric, build_probes, query_probes)
         total_query_time += query_time
         print(f"{recall:.2f}", end=", ")
     print()

fast_pq/__init__.py
@@ -2,4 +2,5 @@ from . import _transform
 from . import _fast_pq
 from .fast_pq import FastPQ
 from .ivf import IVF
+from . import utils
 from .utils import bottom_k, bottom_k_2d, cdist, brute, group_data_by_indices

fast_pq/ivf.py
@@ -76,6 +76,8 @@ class IVF:
             n_probes <= self.n_clusters
         ), f"Can't assign points to {n_probes} clusters, as index only has {self.n_clusters}"
         self.data = data = X.copy()
+        if self.metric == "angular":
+            data /= np.linalg.norm(data, axis=1, keepdims=True)
 
         with timer(verbose, "Computing nearest clusters..."):
             nearest_indices = brute(data, self.all_centers, k=n_probes, metric=self.metric)
@@ -84,9 +86,6 @@ class IVF:
             # We make sure that all centers have at least one point. This is just
             # a simple optimiation that's useful when there's not a lot of data
             # in the IVF, or if the query is OOD.
-            print(nearest_indices.shape)
-            print(nearest_indices)
-            print(np.unique(nearest_indices).shape)
             self.active_centers = np.ascontiguousarray(
                 self.all_centers[np.unique(nearest_indices)], dtype=np.float32
             )

fast_pq/utils.py
@@ -72,7 +72,7 @@ def brute(X, Y, k, metric="euclidean", chunk=100):
     if metric == "angular":
         X = X / np.linalg.norm(X, axis=1, keepdims=True)
         Y = Y / np.linalg.norm(Y, axis=1, keepdims=True)
-    elif metric not in ["angular", "euclidean"]:
+    elif metric != "euclidean":
         raise ValueError(f"Metric not supported: {metric}")
     n = X.shape[0]
     res = np.zeros((n, k), dtype=int)

tests/test_multiprobe.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 from fast_pq import IVF, FastPQ, brute
 
@@ -27,7 +28,8 @@ def compute_recall(metric, build_probes, query_probes):
     return recall_at / nq / at
 
 
-def _test_monotone(metric):
+@pytest.mark.parametrize("metric", ["angular", "euclidean"])
+def test_monotone(metric):
     n = 4
     table = []
     for build_probes in range(1, n + 1):
@@ -49,10 +51,17 @@ def _test_monotone(metric):
         for j in range(1, n):
             assert table[i][j] >= table[i][j - 1] - 0.1
 
+@pytest.mark.parametrize("metric", ["angular", "euclidean"])
+def test_good(metric):
+    n = 1000
+    d = 10
+    nq = 30
+    at = 10
+    dpb = 2
+    max_probes = 10
 
-def test_euclidean():
-    _test_monotone("euclidean")
+    X = np.random.randn(n, d).astype(np.float32)
+    qs = np.random.randn(nq, d).astype(np.float32)
 
-
-def test_angular():
-    _test_monotone("angular")
+    assert compute_recall(metric, build_probes=4, query_probes=10) >= .9
+    assert compute_recall(metric, build_probes=10, query_probes=4) >= .9